058abdf12e44924cac508092eedefc4d763718cc
Igor Pashev <pashev.igor@gmail.com>
2025-01-24 19:34:28 +0200

    Add ServiceControl V1 API
    Signed-off-by: Igor Pashev <pashev.igor@gmail.com>

--- gcloud-sdk/src/rest_apis/google_rest_apis/mod.rs
+++ gcloud-sdk/src/rest_apis/google_rest_apis/mod.rs
@@ -4,11 +4,14 @@ pub mod bigquery_v2;
 #[cfg(any(feature = "google-rest-compute-v1"))]
 pub mod compute_v1;
 
+#[cfg(any(feature = "google-rest-dns-v1"))]
+pub mod dns_v1;
+
 #[cfg(any(feature = "google-rest-lustre-v1alpha"))]
 pub mod lustre_v1alpha;
 
-#[cfg(any(feature = "google-rest-dns-v1"))]
-pub mod dns_v1;
+#[cfg(any(feature = "google-rest-servicecontrol-v1"))]
+pub mod servicecontrol_v1;
 
 #[cfg(any(feature = "google-rest-sqladmin-v1"))]
 pub mod sqladmin_v1;
